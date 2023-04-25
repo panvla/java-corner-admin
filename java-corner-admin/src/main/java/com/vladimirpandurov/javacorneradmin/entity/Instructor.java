dcb7618698471baa20549883001a17ce80fda802
panvla <pandvlado@gmail.com>
2023-04-25 21:13:34 +0200

    jpa mapping done.

--- java-corner-admin/src/main/java/com/vladimirpandurov/javacorneradmin/entity/Instructor.java
+++ java-corner-admin/src/main/java/com/vladimirpandurov/javacorneradmin/entity/Instructor.java
@@ -10,13 +10,21 @@ import java.util.Set;
 public class Instructor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="instructor_id", nullable = false)
     private Long instructorId;
+    @Basic
+    @Column(name="first_name", nullable = false, length = 45)
     private String firstName;
+    @Basic
+    @Column(name = "last_name", nullable = false, length = 45)
     private String lastName;
+    @Basic
+    @Column(name= "summary", nullable = false, length = 64)
     private String summary;
-    @OneToMany
+    @OneToMany(mappedBy = "instructor", fetch = FetchType.LAZY)
     private Set<Course> courses = new HashSet<>();
-
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name="user_id", referencedColumnName="user_id", nullable = false)
     private User user;
 
     public Instructor() {
--- java-corner-admin/src/main/java/com/vladimirpandurov/javacorneradmin/entity/Role.java
+++ java-corner-admin/src/main/java/com/vladimirpandurov/javacorneradmin/entity/Role.java
@@ -1,14 +1,21 @@
 package com.vladimirpandurov.javacorneradmin.entity;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table(name = "roles")
 public class Role {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
     private Long roleId;
+    @Basic
+    @Column(name = "name", nullable = false, length = 45, unique = true)
     private String name;
-
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     private Set<User> users = new HashSet<>();
 
     public Role() {
