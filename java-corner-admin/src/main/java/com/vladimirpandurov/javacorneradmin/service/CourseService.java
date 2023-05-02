900b248c15aab86a161e8faa4dcba3bb0cb36446
panvla <pandvlado@gmail.com>
2023-05-02 13:33:32 +0200

    added courseService impl.

---
+++ java-corner-admin/src/main/java/com/vladimirpandurov/javacorneradmin/service/CourseService.java
@@ -0,0 +1,19 @@
+package com.vladimirpandurov.javacorneradmin.service;
+
+import com.vladimirpandurov.javacorneradmin.dto.CourseDTO;
+import com.vladimirpandurov.javacorneradmin.entity.Course;
+import org.springframework.data.domain.Page;
+
+public interface CourseService {
+
+    Course loadCourseById(Long courseId);
+    CourseDTO createCourse(CourseDTO courseDTO);
+    CourseDTO updateCourse(CourseDTO courseDTO);
+    Page<CourseDTO> findCoursesByCourseName(String keyword, int page, int size);
+    void assignStudentToCourse(Long courseId, Long studentId);
+    Page<CourseDTO> fetchCoursesForStudent(Long studentId, int page, int size);
+    Page<CourseDTO> fetchNonEnrolledInCoursesForStudent(Long studentId, int page, int size);
+    void removeCourse(Long courseId);
+    Page<CourseDTO> fetchCoursesForInstructor(Long instructorId, int page ,int size);
+
+}
